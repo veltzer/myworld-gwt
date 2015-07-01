f81a62f0cad8d3b4ad01a5408b5b12b33998269a
Mark Veltzer <mark.veltzer@gmail.com>
2015-07-01 16:10:59 +0300

    small mods to the script

--- scripts/eclipse_gwt.sh
+++ scripts/eclipse_gwt.sh
@@ -2,6 +2,4 @@
 
 # before running this script make sure that you have eclipse for java installed
 # in ~/install/eclipse-gwt (may be soft link).
-#~/install/eclipse-gwt/eclipse -data ~/workspace-gwt 1> /dev/null 2> /dev/null &
-~/install/eclipse-gwt/eclipse -data ~/workspace-gwt
-#~/install/eclipse-gwt/eclipse -data ~/workspace-gwt &
+~/install/eclipse-gwt/eclipse -data ~/workspace-gwt 1> /dev/null 2> /dev/null &
