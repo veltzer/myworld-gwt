1213e9d3ef32f7f7e1145dcdc0d2a63c2e26b83c
Mark Veltzer <mark.veltzer@gmail.com>
2016-06-03 08:49:41 +0300

    new versions of stuff

--- scripts/source_me.sh
+++ scripts/source_me.sh
@@ -1,18 +1,18 @@
 #source this file!!!
 
 # java stuff
-export JAVA_VERSION=1.7.0_80
+export JAVA_VERSION=1.8.0_92
 export JAVA_HOME=$(path_abs ~/install/jdk$JAVA_VERSION)
 export PATH=$(path_prefix "$JAVA_HOME/bin" "$PATH")
 export MANPATH=$(path_prefix "$JAVA_HOME/man" "$MANPATH")
 
 # maven stuff
-export MVN_VERSION=3.3.3 # my own version
+export MVN_VERSION=3.3.9 # my own version
 export M2_HOME=$(path_abs ~/install/apache-maven-$MVN_VERSION)
 export PATH=$(path_prefix "$M2_HOME/bin" "$PATH")
 
 # ant stuff
-export ANT_VERSION=1.9.5
+export ANT_VERSION=1.9.7
 export ANT_HOME=$(path_abs ~/install/apache-ant-$ANT_VERSION)
 export PATH=$(path_prefix "$ANT_HOME/bin" "$PATH")
 
@@ -25,11 +25,10 @@ export GWT_VERSION=2.7.0
 export GWT_HOME=$(path_abs ~/install/gwt-$GWT_VERSION)
 
 # gxt stuff
-export GXT_VERSION=3.1.1-gpl
+export GXT_VERSION=4.0.0-gpl
 export GXT_HOME=$(path_abs ~/install/gxt-$GXT_VERSION)
 
 # tomcat stuff
-export TOMCAT_VERSION=8.0.23
+export TOMCAT_VERSION=8.0.35
 export TOMCAT_HOME=$(path_abs ~/install/apache-tomcat-$TOMCAT_VERSION)
 export PATH=$(path_prefix "$TOMCAT_HOME/bin" "$PATH")
-
